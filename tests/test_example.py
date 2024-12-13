124ac035edc1d5081c9c4e0190b310762b51e91d
Rizalramzi <rizalramzi0@gmail.com>
2024-12-13 17:12:39 +0700

    Repair CI Workflows

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,14 @@
 import unittest
+from app.main import add, subtract
 
-class TestExample(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(2 + 2, 4)
+class TestMainFunctions(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, -1), -2)
 
-    def test_subtract(self):
-        self.assertEqual(4 - 2, 2)
+    def test_subtraction(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 1), -1)
 
 if __name__ == "__main__":
     unittest.main()
